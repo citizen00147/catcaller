153b5fc1140d5322685b69d6ef30a2dc369c2b13
concerned citizen <85201967+citizen00147@users.noreply.github.com>
2021-10-06 18:56:58 -0400

    add filter options and  modify README with changes and social links

--- README.md
+++ README.md
@@ -1,24 +1,25 @@
-# Catcaller :cat:
+<img src="https://i.imgur.com/2viNAxA.png" height=75>
 
-Catcaller is a simple, modern looking cat name generator. Click 'generate' to generate a random name from a list.
+[Catcaller](https://catcaller.netlify.app) is a simple, modern looking cat name generator. Choose from the (3) options and then click 'generate' to generate a random name from a list.
 
 # Roadmap
 
-## **V1.0**
+## **v1.0**
 
-More than 100 possible names to generate.
+- More than 100 possible names to generate.
 
-### &#8595;
+## **v1.0.1**
 
-## **V1.X**
+- Adjusted the padding of the generated name so that longer names appear one a single line.
+- Removed the version number from site title.
 
-Will add more names throughout smaller updates.
+## **v1.1**
 
-### &#8595;
+- Added option to filter names based on sex
+- More names
 
-## **V2.0**
+## **Future updates:**
 
-- Add option to filter names based on sex
 - Add option to favorite names
 
 # Support :speaking_head:
@@ -26,6 +27,11 @@ Will add more names throughout smaller updates.
 Bugs can be reported to <developer@cmdme.dev>
 
 Please reach out with any questions, comments or suggestions
+
+<p>
+<a href="https://twitter.com/citizen00147"><img src="https://img.shields.io/badge/twitter-%231DA1F2.svg?&style=for-the-badge&logo=twitter&logoColor=white" height=25></a> 
+<a href="https://hashnode.com/@citizen00147"><img src="https://img.shields.io/badge/Hashnode-2962FF?style=for-the-badge&logo=hashnode&logoColor=white" height=25></a>
+<p>
 
 # Acknowledgements
 
--- index.js
+++ index.js
@@ -2,29 +2,16 @@ const btn = document.querySelector("button");
 
 document.querySelector("h2").style.visibility = "hidden";
 
-btn.addEventListener("click", function () {
-  document.querySelector("h2").style.visibility = "visible";
-  let catName = names[Math.floor(Math.random() * names.length)];
-  document.querySelector("h2").innerHTML = catName;
-});
-
-let names = [
-  "Gerald",
-  "O'Reginold",
+// An array of neutral names
+let listOfNeutralNames = [
   "Tuna",
   "Starfish",
-  "Luna",
   "Big Tuna",
-  "Merlin",
   "Miyazaki",
   "Crunchums",
-  "Harold",
   "MuMu",
-  "Geraldine",
   "Pixel",
   "Rex",
-  "Hayo",
-  "Parker",
   "Catmeat",
   "Toast",
   "Muffin",
@@ -32,57 +19,40 @@ let names = [
   "Java",
   "Whiskey",
   "Buttercup",
-  "Blossom",
-  "Skeeter",
   "Tickles",
-  "Jasper",
   "Stardust",
   "Cheddar",
-  "Larry",
   "Crispy",
   "Mint",
-  "Carl",
   "Cheeseburger",
   "Sauce",
   "Bear",
   "Roadrash",
   "Skid",
   "Skippy",
-  "Lars",
   "Snot",
   "Sass",
   "Lard",
   "Pickles",
   "Lettuce",
   "Sprinkles",
-  "Bubba",
-  "Charlie",
   "Croc",
-  "Kevin",
-  "Lucifer",
   "Paprika",
   "Robin",
   "Lemon",
   "Cake",
-  "Julius",
-  "Maxwell",
-  "Karen",
   "Salmon",
   "Trout",
-  "Clementine",
   "Scratchy",
   "Garbage",
   "Ambrosia",
   "Pudding",
   "Sushi",
-  "Jerry",
   "Rogue",
   "Cookiedough",
   "Roach",
   "Thickums",
   "Petal",
-  "Boomer",
-  "Comstock",
   "Weasel",
   "Honey Mustard",
   "Ranch",
@@ -91,12 +61,9 @@ let names = [
   "Pepper",
   "Wednesday",
   "Tiny Head",
-  "Jake",
   "Indy",
   "Biscuit",
   "Dreamsicle",
-  "Ziggy",
-  "Jareth",
   "Thin Mint",
   "Grumps",
   "Crinkle",
@@ -116,3 +83,149 @@ let names = [
   "Peanut",
   "Gordita",
 ];
+
+// An array of female names
+let listOfFemaleNames = [
+  "Luna",
+  "Geraldine",
+  "Karen",
+  "Clementine",
+  "Blossom",
+  "Lilac",
+  "Gwen",
+  "Piper",
+  "Barb",
+  "Lilith",
+  "Sandy",
+  "Willow",
+  "Polly",
+  "Vampira",
+];
+
+// An array of male names
+let listOfMaleNames = [
+  "Gerald",
+  "O'Reginold",
+  "Merlin",
+  "Harold",
+  "Hayo",
+  "Parker",
+  "Skeeter",
+  "Carl",
+  "Larry",
+  "Jasper",
+  "Lars",
+  "Bubba",
+  "Charlie",
+  "Kevin",
+  "Julius",
+  "Maxwell",
+  "Jerry",
+  "Boomer",
+  "Comstock",
+  "Lucifer",
+  "Jake",
+  "Jareth",
+  "Ziggy",
+];
+
+//Check if neutral is selected, then generate random neutral gendered names
+function checkNeutral() {
+  const neutralNames = document.getElementById("neutral");
+  const femaleNames = document.getElementById("female");
+  const maleNames = document.getElementById("male");
+  const maleChoice = document.getElementById("maleChoice");
+  const femaleChoice = document.getElementById("femaleChoice");
+  if (neutralNames.checked == true) {
+    btn.disabled = false;
+    femaleChoice.style.textDecoration = "line-through";
+    femaleNames.disabled = true;
+    femaleNames.style.opacity = "15%";
+    maleChoice.style.textDecoration = "line-through";
+    maleNames.disabled = true;
+    maleNames.style.opacity = "15%";
+    btn.addEventListener("click", function () {
+      document.querySelector("h2").style.visibility = "visible";
+      let catName =
+        listOfNeutralNames[
+          Math.floor(Math.random() * listOfNeutralNames.length)
+        ];
+      document.querySelector("h2").innerHTML = catName;
+    });
+  } else {
+    document.querySelector("h2").style.visibility = "hidden";
+    btn.disabled = true;
+    femaleChoice.style.textDecoration = "none";
+    femaleNames.disabled = false;
+    femaleNames.style.opacity = "100%";
+    maleChoice.style.textDecoration = "none";
+    maleNames.disabled = false;
+    maleNames.style.opacity = "100%";
+  }
+}
+
+//Check if female is selected, then generate random female names
+function checkFemale() {
+  const femaleNames = document.getElementById("female");
+  const maleNames = document.getElementById("male");
+  const neutralNames = document.getElementById("neutral");
+  const maleChoice = document.getElementById("maleChoice");
+  const neutralChoice = document.getElementById("neutralChoice");
+  if (femaleNames.checked == true) {
+    btn.disabled = false;
+    neutralChoice.style.textDecoration = "line-through";
+    neutralNames.disabled = true;
+    neutralNames.style.opacity = "15%";
+    maleChoice.style.textDecoration = "line-through";
+    maleNames.disabled = true;
+    maleNames.style.opacity = "15%";
+    btn.addEventListener("click", function () {
+      document.querySelector("h2").style.visibility = "visible";
+      let catName =
+        listOfFemaleNames[Math.floor(Math.random() * listOfFemaleNames.length)];
+      document.querySelector("h2").innerHTML = catName;
+    });
+  } else {
+    document.querySelector("h2").style.visibility = "hidden";
+    btn.disabled = true;
+    neutralChoice.style.textDecoration = "none";
+    neutralNames.disabled = false;
+    neutralNames.style.opacity = "100%";
+    maleChoice.style.textDecoration = "none";
+    maleNames.disabled = false;
+    maleNames.style.opacity = "100%";
+  }
+}
+
+//Check if male is selected, then generate random male names
+function checkMale() {
+  const maleNames = document.getElementById("male");
+  const neutralNames = document.getElementById("neutral");
+  const femaleNames = document.getElementById("female");
+  const femaleChoice = document.getElementById("femaleChoice");
+  const neutralChoice = document.getElementById("neutralChoice");
+  if (maleNames.checked == true) {
+    btn.disabled = false;
+    neutralChoice.style.textDecoration = "line-through";
+    neutralNames.disabled = true;
+    neutralNames.style.opacity = "15%";
+    femaleChoice.style.textDecoration = "line-through";
+    femaleNames.disabled = true;
+    femaleNames.style.opacity = "15%";
+    btn.addEventListener("click", function () {
+      document.querySelector("h2").style.visibility = "visible";
+      let catName =
+        listOfMaleNames[Math.floor(Math.random() * listOfMaleNames.length)];
+      document.querySelector("h2").innerHTML = catName;
+    });
+  } else {
+    document.querySelector("h2").style.visibility = "hidden";
+    btn.disabled = true;
+    neutralChoice.style.textDecoration = "none";
+    neutralNames.disabled = false;
+    neutralNames.style.opacity = "100%";
+    femaleChoice.style.textDecoration = "none";
+    femaleNames.disabled = false;
+    femaleNames.style.opacity = "100%";
+  }
+}
